4eb88d2cf7f60f70814e1cc4e4b1556bd3fbebe7
fepols-cyber <fepols@web.de>
2026-08-18 09:31:39 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='mercatura-v011';const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
